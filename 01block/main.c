3d65c0942a2e2d24fa493eec985fa744bc56fb3e
chinyuchan <chinyuchan@163.com>
2024-05-18 13:48:56 +0800

    block io

---
+++ 01block/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(void) {
+    char buff[1024] = { 0 };
+    int fd = open("/home/chinyuchan/Downloads/ideaIU-2024.1.1.tar.gz", O_RDONLY, 06666);
+    if (fd < 0) {
+        perror("open");
+        return -1;
+    }
+
+    ssize_t sz;
+    while((sz = read(fd, buff, sizeof(buff))) > 0) {
+        printf("<<<<<: %s\n", buff);
+    }
+    if (sz < 0) {
+        perror("read");
+        return -1;
+    }
+
+
+    return 0;
+}
